fc102f5a57fc35f40bccb9b3323fbc63f74a24a7
rajendra-chimala <rajendrachimala5@gmail.com>
2025-04-10 20:31:04 +0545

    commit

---
+++ dayone.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# Creating arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Arithmetic operations
+sum_ab = a + b
+product = a * b
+
+# Matrix
+matrix = np.array([[1, 2], [3, 4]])
+
+# Shape and size
+matrix.shape     # (2, 2)
+matrix.size      # 4
+
+# Useful functions
+np.mean(a)
+np.max(b)
+np.zeros((2, 3))
+np.ones(5)
